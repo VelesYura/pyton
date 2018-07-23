3645e57e5b58864371e9af345fb5b0c8e219142f
y.veles <y.veles@gmail.com>
2018-07-23 10:47:12 +0300

    Выделен класс для работы с группами

--- test/test_adn_group.py
+++ test/test_adn_group.py
@@ -14,12 +14,12 @@ def app(request):
 
 def test_adn_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="Name", header="header", footer="footer"))
+    app.group.create(Group(name="Name", header="header", footer="footer"))
     app.session.logout()
 
 
 def test_adn_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
