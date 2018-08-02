e195049947b69faf5babae794d48e19307353186
VelesYura <40406008+VelesYura@users.noreply.github.com>
2018-08-02 23:09:06 +0300

    3_2 fixture Изменил conftest, проверка на корректность работы браузера

--- test/test_adn_group.py
+++ test/test_adn_group.py
@@ -4,13 +4,9 @@ from model.group import Group
 
 
 def test_adn_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="Name", header="header", footer="footer"))
-    app.session.logout()
 
 
 def test_adn_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
 
